25a454f00e9929b617ff5becbdc937746973b876
Rubén Seco <ruben.seco.rs@gmail.com>
2025-10-12 14:11:41 -0300

    last modification

--- Program.cs
+++ Program.cs
@@ -7,7 +7,6 @@ using ApiEcommerce.Repository.IRepository;
 using Asp.Versioning;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
